68c258a6d758a525c79bf76f1c9373c4080641b9
Winter <riporbofwinter@gmail.com>
2020-01-26 19:02:25 +0100

    start on CompGenerator

--- CompositionGenerator/Main.rb
+++ CompositionGenerator/Main.rb
@@ -2,34 +2,45 @@
 require "yaml" 
 # colorize for coloring text
 require 'colorize'
-#To Do:
-# Enums???
+# Objects
+require_relative 'classes/DataGrabber'
+require_relative 'classes/Calculator'
+require_relative 'classes/ChampionList'
 
 # Setup enums
-# direction_options = Direction.new
+champion_options = ChampionList.new
 
-# # Input name + check if there is a save file
-# puts "What is your name?"
-# protag = Player.new(gets.chomp)
-# if File.exist?("saves/#{protag.name.strip}.yml")
-# 	puts "Welcome back #{protag.name}!"
-# 	protag = YAML.load(File.read("saves/#{protag.name.strip}.yml"))
-# else
-# 	protag.inventory = [Array.new(10, EmptyBlock.new), Array.new(10, 0)]
-# 	protag.generateWorld
-# 	puts "Find 5 diamonds and craft a portal to escape!"
-# 	puts "If you are ever unsure what to do type 'help' for more information.\n\n"
-# end
+puts "Welcome to the winrate calculator!"
 
+# puts "\nLoading Data..."
+#     YAML.load(File.read("saves/Annie.yml"))
+# puts "Done!\n"
+
+# Create objects
+calculator = Calculator.new
+data = DataGrabber.new
+championId = 1
+
+
+# champion = Champion.new(Annie)
 # game start
 loop do 
 	print "Typ your command: "
     input = gets.chomp.downcase.strip
     if input == "quit" || input == "stop" || input == "exit"
     	break  	
+    elsif input == "input"
+        # File.open("saves/Annie.yml", "w") { |file| file.write(champion.to_yaml) }
+        puts "File open"
+    elsif input == "load"
+        champion_options.each do |item| puts "#{item[1]}"
+        puts champion_options.count
+
+        # if champion = champion_options.find{ |item| item[0] == championId}
+            # puts champion[1]
+        end
     elsif input == "help"
-    	 puts "\nList of Commmands: quit, .\n"
-    =
+    	 puts "\nList of Commmands: quit, input, load.\n"
     else
     	puts "Not an acceptable command, try 'help' for the list of commands."
     end
@@ -51,4 +62,4 @@ end
 # 	else
 # 		puts "Please anwser with either yes(y) or no(n)"
 # 	end
-end
+# end
